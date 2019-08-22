ad860b8280d402cf2f9e08e5552dd5f016e5e7a7
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-22 23:40:49 +0200

    Create new OutputTarget inplementation which save report to html file

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,15 @@ import { MatchReader } from './MatchReader';
 import { Summary } from './Summary';
 import { WinAnalysis } from './analyzers/WinsAnalysis';
 import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { HtmlReports } from './reportTargets/HtmlReport';
 
 const reader = new MatchReader('football.csv');
 reader.read();
 
 const summary = new Summary(new WinAnalysis('Everton'), new ConsoleReport());
 summary.buildAndPrintReport(reader.data);
+
+new Summary(
+	new WinAnalysis('Man United'),
+	new HtmlReports()
+).buildAndPrintReport(reader.data);
